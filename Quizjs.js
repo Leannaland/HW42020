8843d30277ddb5d2905f47d72073f93186cbe09b
Leannaland <leannaland2011@gmail.com>
2020-09-22 20:46:03 -0400

    working onclick

--- Quizjs.js
+++ Quizjs.js
@@ -1,12 +1,7 @@
 
-// Click Event to start quiz //
-
-$( "#quizstartbtn" ).click(function() {
-  $("quiz").click();
-});
  // variables 
 
-var startup = document.getElementById("startup");
+var startup = document.getElementById("quiz-header");
 var quiz = document.getElementById("questionDiv");
 var scores = document.getElementById("scoresDiv");
 var question = document.getElementById("question")
@@ -14,8 +9,10 @@ var options1 = document.getElementById("a");
 var options2 = document.getElementById("b");
 var options3 = document.getElementById("c");
 var options4 = document.getElementById("b");
-var Points = document.getElementById("Points");
-var seconds = 90;
+
+function myFunction() {
+  document.getElementById("quiz-header").style.display = "none";
+
 
 //Questions
   var QuizQuestions = [
@@ -55,40 +52,6 @@ var seconds = 90;
 console.log(QuizQuestions)
 
 
-//functionaility 
-var previousQuestion = questions.length - 1;
-var displayedQuestions = 0;
-
-
-function randomQuestions() {
-    var randomQuestions = question[displayedQuestions];
-    question.innerHTML = "<h5>" + randomQuestions.question + "</h5>";
-    options1.innerHTML = randomQuestions.options1;
-    options2.innerHTML = randomQuestions.options2;
-    options3.innerHTML = randomQuestions.options3;
-    options4.innerHTML = randomQuestions.options4;
 }
 
-function countDown() {
-    var timeLeft = document.getElementById("time")
-    var i = setInterval(function () {
-        seconds--;
-        var secondsString = seconds.toString();
-        timeLeft.textContent = secondsString;
-        if (seconds <= 0) {
-            clearInterval(i);
-            alert("Out of Time")
-        } else if (previousQuestion === displayedQuestions) {
-            clearInterval(i)
-        }
-    }, 1000);
-}
-
-document.getElementById("quizstartbtn").addEventListener("click", function startQuiz() {
-  startup.style.display = "none";
-    randomQuestions();
-    quiz.style.display = "block"
-    countDown();
-});
-
-
+//
